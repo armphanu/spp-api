eb704d78ab78337b92d81d3994b3a1f94314d4f2
armphanu <armzazgakkub@gmail.com>
2023-05-20 12:41:56 +0700

    cors enable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model.model import predict_pipeline
 app = FastAPI()
@@ -15,6 +16,15 @@ class SPP(BaseModel):
     pg : int
     type: str
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
